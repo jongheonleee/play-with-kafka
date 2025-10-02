b01b368e6cb49662258cc981c1cfdcfb2c1cfebd
jongheonleee <qwefghnm1212@gmail.com>
2025-10-02 17:42:21 +0900

    카프카야 노올자자아아아 ㅋㅋㅋ

---
+++ src/test/java/com/example/play_with_kafka/PlayWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.play_with_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
